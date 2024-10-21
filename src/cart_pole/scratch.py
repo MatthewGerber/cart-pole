0fdc5f181cfda1ceb7aaca01c09b042371e4c69e
MatthewGerber <gerber.matthew@gmail.com>
2024-10-20 22:49:41 -0400

    Scratch.

--- src/cart_pole/scratch.py
+++ src/cart_pole/scratch.py
@@ -1,7 +1,10 @@
 import time
 
+from smbus2 import SMBus
+import RPi.GPIO as gpio
 from raspberry_py.gpio import CkPin, setup, cleanup
-from raspberry_py.gpio.lights import LED
+from raspberry_py.gpio.integrated_circuits import PulseWaveModulatorPCA9685PW
+from raspberry_py.gpio.motors import DcMotor, DcMotorDriverIndirectPCA9685PW
 
 
 def main():
@@ -10,17 +13,36 @@ def main():
     """
 
     setup()
-    leds = [
-        LED(CkPin.GPIO26),
-        LED(CkPin.GPIO19),
-        LED(CkPin.GPIO13)
-    ]
-    for _ in range(10):
-        for led in leds:
-            led.turn_on()
-            time.sleep(0.01)
-            led.turn_off()
-            time.sleep(0.01)
+
+    gpio.setup(CkPin.GPIO6, gpio.OUT)
+    gpio.output(CkPin.GPIO6, gpio.LOW)
+
+    pca9685pw = PulseWaveModulatorPCA9685PW(
+        bus=SMBus('/dev/i2c-1'),
+        address=PulseWaveModulatorPCA9685PW.PCA9685PW_ADDRESS,
+        frequency_hz=100
+    )
+    motor = DcMotor(
+        driver=DcMotorDriverIndirectPCA9685PW(
+            pca9685pw=pca9685pw,
+            pwm_channel=0,
+            direction_pin=CkPin.GPIO21,
+            reverse=True
+        ),
+        speed=0
+    )
+    motor.start()
+
+    try:
+        while True:
+            motor.set_speed(0)
+            time.sleep(1.0)
+            motor.set_speed(100)
+            time.sleep(0.00001)
+    except KeyboardInterrupt:
+        pass
+
+    motor.set_speed(0)
     cleanup()
 
 
