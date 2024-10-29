d83cd500f27142152a6fec739c5b7009086fa564
MatthewGerber <gerber.matthew@gmail.com>
2024-10-28 23:19:31 -0400

    Add scaling per state segment.

--- src/cart_pole/feature_extraction.py
+++ src/cart_pole/feature_extraction.py
@@ -6,7 +6,7 @@ import numpy as np
 
 from cart_pole.environment import CartPoleState, CartPole, EpisodePhase
 from rlai.core import MdpState
-from rlai.models.feature_extraction import FeatureExtractor, StationaryFeatureScaler
+from rlai.models.feature_extraction import FeatureExtractor
 from rlai.state_value.function_approximation.models.feature_extraction import (
     StateFeatureExtractor,
     OneHotStateIndicatorFeatureInteracter,
@@ -74,13 +74,15 @@ class CartPolePolicyFeatureExtractor(StateFeatureExtractor):
         :param environment: Environment.
         """
 
-        super().__init__()
+        super().__init__(True)
 
         self.environment = environment
 
-        self.scaler = StationaryFeatureScaler()
-        self.state_category_interacter = self.get_interacter()
         self.interaction_term_indices: Optional[List[Tuple]] = None
+        (
+            self.state_category_feature_interacter,
+            self.state_category_intercept_interacter
+        ) = self.get_interacters()
 
     def __getstate__(
             self
@@ -93,7 +95,8 @@ class CartPolePolicyFeatureExtractor(StateFeatureExtractor):
 
         state = dict(self.__dict__)
 
-        state['state_category_interacter'] = None
+        state['state_category_feature_interacter'] = None
+        state['state_category_intercept_interacter'] = None
 
         return state
 
@@ -109,7 +112,10 @@ class CartPolePolicyFeatureExtractor(StateFeatureExtractor):
 
         self.__dict__ = state
 
-        state['state_category_interacter'] = self.get_interacter()
+        (
+            self.state_category_feature_interacter,
+            self.state_category_intercept_interacter
+        ) = self.get_interacters()
 
     def extracts_intercept(
             self
@@ -153,7 +159,7 @@ class CartPolePolicyFeatureExtractor(StateFeatureExtractor):
         # range all features to be in a nominal range of approximately [-1.0, 1.0]. this is only approximate because
         # the minimum and maximum values of some state dimensions (e.g., pole angular velocity) are unlimited in theory
         # and uncalibrated in practice -- we provide rough estimates manually.
-        sign_matrix = np.array([
+        state_sign_matrix = np.array([
             [
                 np.sign(state.cart_mm_from_center),
                 np.sign(state.cart_velocity_mm_per_second),
@@ -164,7 +170,7 @@ class CartPolePolicyFeatureExtractor(StateFeatureExtractor):
             for state in states
             if isinstance(state, CartPoleState)
         ])
-        zero_to_one_feature_matrix = np.array([
+        zero_to_one_state_matrix = np.array([
             [
                 state.zero_to_one_cart_distance_from_center,
                 state.zero_to_one_cart_speed,
@@ -177,66 +183,62 @@ class CartPolePolicyFeatureExtractor(StateFeatureExtractor):
         ])
 
         # invert back to 1.0 being most physically extreme
-        ranged_feature_matrix = sign_matrix * (1.0 - zero_to_one_feature_matrix)
+        ranged_state_matrix = state_sign_matrix * (1.0 - zero_to_one_state_matrix)
 
         # create the full matrix of multiplicative interaction terms between the ranged feature values
-        interaction_feature_matrix = np.array([
+        state_feature_matrix = np.array([
             [
-                np.prod(row[term_indices])
+                np.prod(ranged_feature_vector[term_indices])
                 for term_indices in self.interaction_term_indices
             ]
-            for row in ranged_feature_matrix
+            for ranged_feature_vector in ranged_state_matrix
         ])
 
-        state_indicator_matrix = np.array([
+        state_matrix = np.array([
             state.observation
             for state in states
             if isinstance(state, CartPoleState)
         ])
 
-        # interact the feature matrix according to its state segment
-        state_indicator_feature_matrix = self.state_category_interacter.interact(
-            state_indicator_matrix,
-            interaction_feature_matrix
-        )
-
-        # ranging the feature values to be in [-1.0, 1.0] according to their theoretical bounds doesn't mean that the
-        # distribution of observed values will be similar when running. for example, the observed cart and pole
-        # velocities might be quite small relative to their ranges and the observed values of cart and pole positions.
-        # these differences in observed distribution across features result in the usual issues pertaining to step
-        # sizes in the policy updates. standardize the ranged feature matrix so that a single step size will be
-        # suitable for learning. we scale here, after interaction, to ensure that observations in each state segment
-        # are scaled according to their naturally observed distributions.
-        scaled_state_indicator_feature_matrix = self.scaler.scale_features(
-            state_indicator_feature_matrix,
+        # interact the feature matrix according to its state segment. this will scale the feature values within their
+        # segments along the way. why scaling? ranging the feature values to be in [-1.0, 1.0] according to their
+        # theoretical bounds doesn't mean that the distribution of observed values will be similar when running. for
+        # example, the observed cart and pole velocities might be quite small relative to their ranges and the observed
+        # values of cart and pole positions. these differences in observed distribution across features result in the
+        # usual issues pertaining to step sizes in the policy updates. standardize the ranged feature matrix so that a
+        # single step size will be suitable for learning.
+        scaled_state_indicator_feature_matrix = self.state_category_feature_interacter.interact(
+            state_matrix,
+            state_feature_matrix,
             refit_scaler
         )
 
         # prepend a vector of intercept terms to each row according to the state segment. we do this here, after
         # scaling, so that the constant intercept terms are not scaled to zero.
-        state_indicator_intercepts = self.state_category_interacter.interact(
-            state_indicator_matrix,
-            0.01 * np.ones((scaled_state_indicator_feature_matrix.shape[0], 1))
+        state_indicator_intercepts = self.state_category_intercept_interacter.interact(
+            state_matrix,
+            np.ones((scaled_state_indicator_feature_matrix.shape[0], 1)),
+            False
         )
 
-        scaled_feature_matrix_with_intercepts = np.append(
+        scaled_state_indicator_feature_matrix_with_intercepts = np.append(
             state_indicator_intercepts,
             scaled_state_indicator_feature_matrix,
             axis=1
         )
 
-        return scaled_feature_matrix_with_intercepts
+        return scaled_state_indicator_feature_matrix_with_intercepts
 
-    def get_interacter(
+    def get_interacters(
             self
-    ) -> OneHotStateIndicatorFeatureInteracter:
+    ) -> Tuple[OneHotStateIndicatorFeatureInteracter, OneHotStateIndicatorFeatureInteracter]:
         """
-        Get interacter.
+        Get interacters.
 
-        :return: Interacter.
+        :return: Interacters, one for features and one for intercepts.
         """
 
-        return OneHotStateIndicatorFeatureInteracter([
+        indicators = [
 
             # segment policy per pole on either side of vertical. we haven't yet found a feature that quantifies the
             # correct policy response for pole angle. the feature would need to have similar values near either side
@@ -258,4 +260,15 @@ class CartPolePolicyFeatureExtractor(StateFeatureExtractor):
                 ) == EpisodePhase.BALANCE,
                 [False, True]
             )
-        ])
+        ]
+
+        # it's important to scale features independently within each state segment, since we'll make many observations
+        # in certain segments early in the learning before ever getting to the other segments (e.g., for balancing). we
+        # don't want the early zero-valued features for unobserved segments (due to one-hot segment encoding) to pollute
+        # the scalers used for segments observed later in learning.
+        feature_interacter = OneHotStateIndicatorFeatureInteracter(indicators, True)
+
+        # do not scale intercepts when encoding them
+        intercept_interacter = OneHotStateIndicatorFeatureInteracter(indicators, False)
+
+        return feature_interacter, intercept_interacter
